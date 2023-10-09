13881504e6a8f85aa7912365608ff945c7ffa824
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-10-09 13:18:59 -0400

    Initial Commit

---
+++ ICalories/ICaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICaloriesApp.swift
+//  ICalories
+//
+//  Created by oyebayo oluwafemi on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct ICaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
