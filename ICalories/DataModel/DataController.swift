9de3b88575e6ef5d987e540771d97ae6caaf8def
oyebayofemi <oyebayooluwafemi@gmail.com>
2024-07-04 19:15:11 -0400

    Complete

---
+++ ICalories/DataModel/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  ICalories
+//
+//  Created by oyebayo oluwafemi on 2023-10-09.
+//
+
+import Foundation
---
+++ ICalories/Views/AddFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  AddFoodView.swift
+//  ICalories
+//
+//  Created by oyebayo oluwafemi on 2023-10-10.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFoodView()
+}
